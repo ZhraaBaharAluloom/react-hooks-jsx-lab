16ae85be353191230503029840bea5f5b27f7b18
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2021-07-25 18:23:55 -0700

    add a jsx

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about" style={{ textAlign: "center", padding: "15px" }}>
+      <h2>About Me</h2>
+      <p> Hello, This is me ^_^ Zahraa</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
